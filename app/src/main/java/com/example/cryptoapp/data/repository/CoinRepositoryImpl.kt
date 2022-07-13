65e49894101be39c7cac0da423df11c7a5dd6841
Shahriyor <shahriyorabdullayev2000@gmail.com>
2022-07-13 17:53:20 +0500

    initial commit

---
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.example.cryptoapp.data.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.database.CoinInfoDao
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.workers.RefreshDataWorker
+import com.example.cryptoapp.domain.CoinInfo
+import com.example.cryptoapp.domain.CoinRepository
+import javax.inject.Inject
+
+class CoinRepositoryImpl @Inject constructor(
+    private val mapper: CoinMapper,
+    private val coinInfoDao: CoinInfoDao,
+    private val application: Application
+) : CoinRepository {
+
+    override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
+        return Transformations.map(coinInfoDao.getPriceList()) {
+            it.map {
+                mapper.mapDbModelToEntity(it)
+            }
+        }
+    }
+
+    override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
+        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
+            mapper.mapDbModelToEntity(it)
+        }
+    }
+
+    override fun loadData() {
+        val workManager = WorkManager.getInstance(application)
+        workManager.enqueueUniqueWork(
+            RefreshDataWorker.NAME,
+            ExistingWorkPolicy.REPLACE,
+            RefreshDataWorker.makeRequest()
+        )
+    }
+}
